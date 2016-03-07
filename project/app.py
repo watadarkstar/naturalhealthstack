ec740f012cf786de6ea22141befa030130ab980a
Adrian Carolli <adrian.carolli1@gmail.com>
2016-03-07 23:04:09 +0000

    started working on gspread sheet

--- project/app.py
+++ project/app.py
@@ -1,17 +1,13 @@
+import json
+import gspread
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
-
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
